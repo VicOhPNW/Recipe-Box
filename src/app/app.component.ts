6d4fc665e1f9f08d8a0364a70c5fa57fd0b95d5d
Mel Yasuda and Victoria Oh <victoria@>
2018-10-23 15:50:21 -0700

    Complete Tuesday lessons for Angular (Recipe Box) in place of to-do.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'recipe-box',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe-Box';
+
+  recipes: Recipe[] = [
+    new Recipe("Hot Dogs", "hot-dog, buns, condiments", "It's easy. Google it, stupid.", 3),
+    new Recipe("Apple", "apple", "just bite into it. simple.", 2),
+    new Recipe("Orange", "orange", "just peel it. simple.", 1)
+  ]
+
+  selectedRecipe = null;
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
+  deliciousnessColor(recipe){
+    if(recipe.deliciousness === 3){
+      return "bg-danger";
+    } else if (recipe.deliciousness === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
