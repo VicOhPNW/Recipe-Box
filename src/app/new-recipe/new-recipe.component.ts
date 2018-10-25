32036a83e9fe6983128baa7e295c4c1dab5c3e6d
Victoria Oh <ohvictori@gmail.com>
2018-10-25 14:59:53 -0700

    Complete lesson 'Template Reference Variables and Creating Objects.'

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-new-recipe',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-recipe.component.css']
 })
 export class NewRecipeComponent implements OnInit {
+  @Output() sendTask = new EventEmitter();
+  
+  submitForm(name: string, ingredients: string, directions: string, deliciousness: string) {
+    let newRecipe = new Recipe(name, ingredients, directions, parseInt(deliciousness));
+    this.sendTask.emit(newRecipe);
+  }
 
   constructor() { }
 
